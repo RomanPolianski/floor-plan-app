e5695ca4e5723d3375e143ae9d2abd5b5e73525e
Roman <rpol123453@gmail.com>
2022-08-27 13:46:38 +0300

    Add Map component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import './App.scss';
+import Map from './components/Content/Map/Map';
+import Header from './components/Header/Header';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
+    <>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.contentLeft}>
+          <h1>People</h1>
+        </div>
+        <div className={styles.contentRight}>
+          <Map />
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Content/Map/Map.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-expressions */
+import { MapContainer, GeoJSON } from 'react-leaflet';
+import { FC, useState } from 'react';
+import ReactLeafletGoogleLayer from 'react-leaflet-google-layer';
+import { mapData } from '../../../../data/floor';
+import 'leaflet/dist/leaflet.css';
+import Button from '../../../common/buttons/close/Button';
+import styles from './Map.module.scss';
+
+const Map: FC = (): JSX.Element => {
+  const [isMapView, setIsMapView] = useState(false);
+  const [textButtonView, setTextButtonView] = useState('Map view');
+
+  const toggleMapView = () => {
+    setIsMapView(!isMapView);
+    !isMapView ? setTextButtonView('Scheme') : setTextButtonView('Map view');
+  };
+
+  return (
+    <div>
+      <MapContainer
+        style={{ height: '80vh' }}
+        zoom={20}
+        center={[53.91713, 27.63483]}
+      >
+        {isMapView && <ReactLeafletGoogleLayer type="hybrid" />}
+        <GeoJSON attribution="&copy; 2022" data={mapData} />
+      </MapContainer>
+      <div className={styles.map__button}>
+        <Button text={textButtonView} onClick={toggleMapView} />
+      </div>
+    </div>
+  );
+};
+
+export default Map;
