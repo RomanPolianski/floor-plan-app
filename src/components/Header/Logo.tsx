2eb8fe20288d9a7b472a70b93066fd95885d82c9
Roman <rpol123453@gmail.com>
2022-08-28 14:32:07 +0300

    Fix Header componenet

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import styles from './Header.module.scss';
 
 const Logo: FC = (): JSX.Element => {
-  return <h1 className={styles.header__title}>FLOOR PLAN</h1>;
+  return <h1 className={styles.header__title}>PEOPLE | FLOOR PLAN</h1>;
 };
 
 export default Logo;
